5828eac8fa816ac0dd5504a97a0592050e708d90
Giosuè Sulipano <34242779+imgios@users.noreply.github.com>
2025-09-05 19:34:06 +0200

    Change the lead paragraph

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,7 @@
 // https://phuoc.ng/collection/css-animation/typewriter/
 
 document.addEventListener("DOMContentLoaded", () => {
-    const content = 'We are simply human beings';
+    const content = 'We are simply human beings, and IT lovers, behind (mechanical) keyboards';
     const textEle = document.getElementById('typing');
     const cursorEle = document.getElementById('cursor');
     let charIndex = 0;
